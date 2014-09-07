04da1eaeb2f042566a67ac4ded27710763a5f07c
Robert Starmer <rstarmer@gmail.com>
2014-09-07 16:12:54 +1000

    Add Gemfile, spec, and inital app

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ ruby_hw
 =======
 
 A simple Ruby Hello World app leveraging Sinatra
+
+Tests are in rspec
+
---
+++ spec/hw_spec.rb
@@ -0,0 +1,9 @@
+# spec/hw_spec.rb
+require File.expand_path '../spec_helper.rb', __FILE__
+
+describe "My Sinatra Application" do
+  it "should allow accessing the home page" do
+    get '/'
+    last_response.should be_ok
+  end
+end
