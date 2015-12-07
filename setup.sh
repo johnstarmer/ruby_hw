ecfdeba493b8cce2549d4200e2063f29c4e3bdfe
Robert Starmer <rstarmer@gmail.com>
2015-12-06 19:45:50 -0600

    Fix setup and bundle command

--- setup.sh
+++ setup.sh
@@ -4,9 +4,6 @@
 
 sudo yum install -y git httpd ruby-devel rubygems epel-release pygpgme curl
 
-# Install the bundler gem so that we can acquire our ruby libraries
-sudo gem install bundler
-
 # Add the phusionsoft passenger RPM repository so that we can install the
 # Apache passenger module
 sudo curl --fail -sSLo /etc/yum.repos.d/passenger.repo \
@@ -16,9 +13,16 @@ sudo curl --fail -sSLo /etc/yum.repos.d/passenger.repo \
 sudo yum install -y mod_passenger
 
 # get the ruby environment from passenger
-# export ruby-cmd=`passenger-config about ruby-command | awk '/Command:/ {print $2}'
+# export ruby-cmd=`passenger-config about ruby-command | awk '/Command:/ {print $2}' | head -1`
 
 # create our app configuration:
+
+sudo git clone https://github.com/rstarmer/ruby_hw -b develop /var/www/ruby_hw
+sudo chown -R centos: /var/www/ruby_hw
+
+# Install the bundler gem so that we can acquire our ruby libraries
+sudo gem install bundler
+(cd /var/www/ruby_hw; /usr/local/bin/bundle install --deployment --without development test)
 
 sudo tee /etc/httpd/conf.d/ruby_hw.conf > /dev/null <<EOF
 <VirtualHost *:80>
@@ -37,10 +41,5 @@ sudo tee /etc/httpd/conf.d/ruby_hw.conf > /dev/null <<EOF
     </Directory>
 </VirtualHost>
 EOF
-
-sudo git clone https://github.com/rstarmer/ruby_hw /var/www/ruby_hw
-sudo chown -R centos: /var/www/ruby_hw
-
-(cd /var/www/ruby_hw; /usr/local/bin/bundle install)
 
 sudo systemctl restart httpd.service
