3a7a94c2612c66320aca0261326b5bdd1a8e77b3
Robert Starmer <rstarmer@gmail.com>
2016-01-05 02:43:34 -0800

    Upate index

--- ruby_hw.tf
+++ ruby_hw.tf
@@ -9,14 +9,11 @@
 
 #  Provision via external ansible, but create the inventory
 resource "digitalocean_droplet" "ruby-hw" {
-    image = "centos-7-0-x64"
+    image = "ubuntu-14-04-x64"
     name = "ruby-hw"
     region = "sfo1"
     size = "512mb"
     ssh_keys = ["778729"]
-    provisioner "local-exec" {
-      command = "echo ${digitalocean_droplet.ruby-hw.ipv4_address} ansible_connection=ssh ansible_ssh_user=root >> inventory"
-    }
 }
 
 # If you don't already have a domain defined in Digital Ocean
